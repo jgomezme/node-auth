62b7e2e3a37aa14f9aad963da8eb4aebe146b1bc
Gomosoft <jgomez@soluntech.com>
2014-12-01 09:27:44 -0500

    init bases

---
+++ api/controllers/controller.base.js
@@ -0,0 +1,29 @@
+// controller base
+
+
+var model = function (server){
+    			
+       function get(req, res, next){
+       	// code
+       }
+
+       function del(req, res, next){
+       	// code
+       }
+
+       function put(req, res, next){
+       	// code
+       }
+
+       function update(req, res, next){
+       	// code
+       }
+
+
+       server.get('',get);
+       server.post('',put);
+       server.put('',put);
+       server.del('',del);
+
+
+}
